f325c0c2f2198b980cbbeafbbc45630791050ef9
Debasish <149887980+RaptorDot@users.noreply.github.com>
2024-01-17 15:57:08 +0000

    conversions of datatypes

---
+++ basics_01/02_ConversionOprations.js
@@ -0,0 +1,24 @@
+let score = "321asb"
+
+console.log(typeof score)
+let newNum = Number(score)
+console.log(typeof newNum)
+console.log(newNum)
+
+let value1 = undefined;
+let value2 = true;
+let value3 = null;let randomNum = 45;
+let strNum = String(randomNum);
+console.log(typeof strNum);
+console.log(strNum)
+
+console.log(value1);
+console.log(typeof value1);
+
+console.log(value2);
+console.log(typeof value2);
+
+console.log(value3);
+console.log(typeof value3);
+
+
--- basics_01/newStart.js
+++ basics_01/newStart.js
@@ -1,1 +1,12 @@
 console.log("hello")
+const userName = "Debasish" // value cannot be changed
+let accountEmail = "XYZ@google.com"
+var accountPassword = "12345"
+// value can be changed, mainly scope make the diff b/w "let"* "var".
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+console.log(accountId);
+console.table([userName,accountEmail,accountPassword])
