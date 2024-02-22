d62a5989620ed844d0e03c1ca9abcdc6f3bc17b7
Debasish <149887980+RaptorDot@users.noreply.github.com>
2024-02-22 07:25:42 +0000

    5

---
+++ basics_01/numbers,maths, and date and time.js
@@ -0,0 +1,8 @@
+//----Number-----
+// Number values represent floating-point numbers like 37 or -9.25.
+
+// The Number constructor contains constants and methods for working with numbers. Values of other types can be converted to numbers using the Number() function.
+
+// When used as a function, Number(value) converts a string or other value to the Number type. If the value can't be converted, it returns NaN.
+let num1 = 6;
+    console.log(num1);
