75c75cb4cf509c1bfa4b627f3dcf34ae7ce5b257
Debasish <149887980+RaptorDot@users.noreply.github.com>
2024-02-24 08:29:28 +0000

    obj

--- basics_02/objects.js
+++ basics_02/objects.js
@@ -14,3 +14,36 @@ console.log(jsUser["location"])
 //we can use object's useing "dot" oparetor or "[]" brakets
 // console.log(jsUser.full name)
 console.log(jsUser["full name"]);//full name cant be use eith dot oparetor in this case
+//overwriting
+
+jsUser.age = 21;
+
+console.log(jsUser.age)
+    // Object.freeze(jsUser); we cannot change any properties after that
+    jsUser.greetings = function () {
+        console.log("hello world")
+    }
+console.log(jsUser.greetings)
+console.log(jsUser.greetings())
+
+    jsUser.greetingsTwo = function() {
+        console.log(`hello user, your name is: ${this["full name"]}`)
+    }
+    console.log(jsUser.greetingsTwo())
+    const compUser = {
+        userFullName: {
+            firstName: "Debasish",
+            lastName: "Das"
+        },
+        email: "jsUser@jmail.com"
+    }
+
+    console.log(compUser);
+    const obj1 = {1:a,2:b,3:c};
+    const obj2 = {4:d,5:e}
+
+    const obj3 = Object.assign({},obj1,obj2)
+    console.log(obj3)
+
+
+
