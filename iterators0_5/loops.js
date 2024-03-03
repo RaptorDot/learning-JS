b8a665b8d8802ec334c705084c452c5859bb2aea
Debasish <149887980+RaptorDot@users.noreply.github.com>
2024-03-03 08:37:44 +0000

    q

--- iterators0_5/loops.js
+++ iterators0_5/loops.js
@@ -1,4 +1,30 @@
 
-for (let i = 0; i < 10; i++) {
-    console.log(`now the value is ${i}` )
+for (let i = 1; i <=10; i++) {
+    // console.log(`now the value is ${i}` )
 }
+//finding a val
+
+let val = 5;
+for (let index = 0; index < 10; index++) {
+    if(index==val) {
+        //console.log(`the value is : ${index}`)
+    }
+} 
+ 
+//---------while loop----------
+// print all even nums b/w 1-10
+let index = 2;
+while (index<10) {
+   // console.log(`The num is: ${index}`);
+    index +=2;
+}
+
+// printing values of a array]
+
+const arr = [1,2,3,74,6,8,75];
+    //using forof loop
+        for (const num of arr) {
+           // console.log(num);
+        }
+    
+
