e3d4f4b113fc003607030926ed8c1f3fec6fc425
Debasish <149887980+RaptorDot@users.noreply.github.com>
2024-03-10 16:50:52 +0000

    loops

--- iterators0_5/loops.js
+++ iterators0_5/loops.js
@@ -68,8 +68,27 @@ const arr = [1,2,3,74,6,8,75];
     ]
 
     objArray.forEach(i => {
-        console.log(i.name);
+      //  console.log(i.name);
     });
 
-    
+    const mNums = [0,10,20,30,40,50,60,70,80,90];
 
+    const nNums = mNums.map(n => n+ 10);
+   // console.log(nNums);
+    
+   const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+
+    const mbooks = books.filter((bk) => bk.genre = "Fiction" && bk.publish >= 2000)
+
+  //console.log(mbooks)
